acf71f19e79955a3a2eef2e6a6164c83fc02bc97
Christine Boersen <Christine.Boersen@live.com>
2015-08-31 09:10:37 -0400

    Initial Commit

---
+++ WakaTimeTaskbar/WakaTimeTaskbar/NativeCalls.cs
@@ -0,0 +1,57 @@
+﻿using Microsoft.Win32;
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WakaTimeTaskbar
+{
+    public static class NativeCalls
+    {
+        public delegate void WinEventDelegate(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime);
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr SetWinEventHook(uint eventMin, uint eventMax, IntPtr hmodWinEventProc, WinEventDelegate lpfnWinEventProc, uint idProcess, uint idThread, uint dwFlags);
+
+        public const uint WINEVENT_OUTOFCONTEXT = 0;
+        public const uint EVENT_SYSTEM_FOREGROUND = 3;
+
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetForegroundWindow();
+
+        [DllImport("user32.dll")]
+        public static extern int GetWindowText(IntPtr hWnd, StringBuilder text, int count);
+
+        [DllImport("user32.dll", SetLastError = true)]
+        public static extern uint GetWindowThreadProcessId(IntPtr hWnd, out uint lpdwProcessId);
+
+        public static string[] GetActiveWindowInfo()
+        {
+            const int nChars = 256;
+            IntPtr handle = IntPtr.Zero;
+            StringBuilder Buff = new StringBuilder(nChars);
+            handle = GetForegroundWindow();
+            uint procId = 0;
+            GetWindowThreadProcessId(handle, out procId);
+            var proc = Process.GetProcessById((int) procId);
+
+            if (GetWindowText(handle, Buff, nChars) > 0)
+            {
+                return new string[] { proc.ProcessName, Buff.ToString() };
+            }
+            return null;
+        }
+        public static void WinEventProc(IntPtr hWinEventHook, uint eventType, IntPtr hwnd, int idObject, int idChild, uint dwEventThread, uint dwmsEventTime)
+        {
+            string[] WindowInfo = GetActiveWindowInfo();
+            if (WindowInfo != null)
+            {
+                Console.WriteLine(string.Format("Process: {0}, Window Title: {1}", WindowInfo[0], WindowInfo[1]));
+            }
+            
+        }
+    }
+}
---
+++ WakaTimeTaskbar/WakaTimeTaskbar/Options.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static WakaTimeTaskbar.NativeCalls;
+
+namespace WakaTimeTaskbar
+{
+    public partial class Options : Form
+    {
+        NativeCalls.WinEventDelegate _windowDelegate = null;
+
+        public Options()
+        {
+            InitializeComponent();            
+            _windowDelegate = new WinEventDelegate(WinEventProc);
+            IntPtr m_hhook = SetWinEventHook(EVENT_SYSTEM_FOREGROUND, EVENT_SYSTEM_FOREGROUND, IntPtr.Zero, _windowDelegate, 0, 0, WINEVENT_OUTOFCONTEXT);
+            timer1.Enabled = true;
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            string[] WindowInfo = GetActiveWindowInfo();
+            if (WindowInfo != null)
+            {
+                Console.WriteLine(string.Format("Process: {0}, Window Title: {1}", WindowInfo[0], WindowInfo[1]));
+            }
+        }
+    }
+}
